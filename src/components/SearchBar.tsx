7bae09b52079a1f470d3f3dffdb48235e82404c9
PeterZajac <zajacp011@gmail.com>
2024-03-15 15:52:16 +0100

    Refactor SearchBar component to improve accessibility and styling

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -39,41 +39,43 @@ const SearchBar: FC<FruitProps> = ({ data }) => {
   const finalClassName = `${inputBaseClasses} ${borderClass}`;
 
   return (
-    <div className="flex">
-      <input
-        type="search"
-        className={finalClassName}
-        placeholder="Search"
-        aria-label="Search"
-        id="exampleFormControlInput2"
-        aria-describedby="button-addon2"
-        value={searchTerm}
-        onChange={handleInputChange}
-      />
-      <button onClick={handleButtonClick}>
-        <span
-          className="flex items-center whitespace-nowrap px-3 py-[0.25rem] text-surface dark:border-neutral-400 dark:text-white [&>svg]:h-5 [&>svg]:w-5"
-          id="button-addon2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
+    <div className="flex flex-col">
+      <div className="flex">
+        <input
+          type="search"
+          className={finalClassName}
+          placeholder="Search"
+          aria-label="Search"
+          id="exampleFormControlInput2"
+          aria-describedby="button-addon2"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleButtonClick}>
+          <span
+            className="flex items-center whitespace-nowrap px-3 py-[0.25rem] text-surface dark:border-neutral-400 dark:text-white [&>svg]:h-5 [&>svg]:w-5"
+            id="button-addon2"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-            />
-          </svg>
-        </span>
-      </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
+          </span>
+        </button>
+      </div>
       {searchResult && (
-        <div>
+        <div className="mt-1">
           {searchResult === "Error: Fruit not found." && (
-            <span className="text-red-500 text-sm mt-1">{searchResult}</span>
+            <span className="text-red-500 text-sm">{searchResult}</span>
           )}
         </div>
       )}
