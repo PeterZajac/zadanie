d2c7b8f2ce47834ee214a97fdbef198d07d7b573
PeterZajac <zajacp011@gmail.com>
2024-03-15 11:20:09 +0100

    Update page.tsx with new container class

---
+++ src/app/list/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListPage = () => {
+  return <div>page List</div>;
+};
+
+export default ListPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello from page.tsx</h1>
+    <div className="container">
+      <h1>cavko</h1>
     </div>
   );
 }
