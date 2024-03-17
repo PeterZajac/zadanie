0599c17f0f681a5c2d1e43faf0218f3fe5121179
PeterZajac <zajacp011@gmail.com>
2024-03-17 21:54:12 +0100

    Add route for getting fruit information

---
+++ src/app/api/get-fruit/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  console.log(req);
+  // get the fruit name from the query string
+  const fruitName = req.nextUrl.searchParams.get("fruit");
+
+  if (!fruitName) {
+    return NextResponse.json(
+      { error: "No fruit name provided" },
+      { status: 400 }
+    );
+  }
+
+  const response = await fetch(
+    `https://www.fruityvice.com/api/fruit/${fruitName}`
+  ).then((res) => res.json());
+
+  return NextResponse.json(response);
+};
