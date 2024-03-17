cc82cab6bac1f190520fd4974a535590ca560df8
PeterZajac <zajacp011@gmail.com>
2024-03-17 21:54:59 +0100

    Add detail page and AddToFavorite component

---
+++ src/app/detail/[id]/page.tsx
@@ -0,0 +1,76 @@
+import { AddToFavorite } from "@/components/AddToFavorite";
+import { TFruit } from "@/types/fruitsType";
+import Image from "next/image";
+import React, { FC } from "react";
+
+type DetailPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const fetchFruit: (id: string) => Promise<TFruit | null> = async (id) => {
+  const res = await fetch(`https://www.fruityvice.com/api/fruit/${id}`);
+  const data: TFruit = await res.json();
+  if (data.error) {
+    return null;
+  }
+  const image = await fetch(
+    `https://api.pexels.com/v1/search?query=${data.name}`,
+    {
+      headers: {
+        Authorization: process.env.IMAGE_API_TOKEN as string,
+      },
+    }
+  );
+  const imageData = await image.json();
+
+  const imageUrl = imageData.photos?.[0].src.original;
+  return { ...data, image: imageUrl };
+};
+
+const DetailPage: FC<DetailPageProps> = async ({ params: { id } }) => {
+  const fruit = await fetchFruit(id);
+
+  if (!fruit) {
+    return <h1>404: Not found</h1>;
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-20">
+      <h4>{fruit.name}</h4>
+      <h1 className="text-[40px] mb-10">DETAIL </h1>
+      {fruit.image && (
+        <Image
+          src={fruit.image}
+          className="h-60 w-60 object-cover rounded-full shadow-lg mb-5"
+          height={200}
+          width={200}
+          alt="Tomato image"
+        />
+      )}
+
+      <div className="flex flex-col rounded-lg p-4  mt-7 w-[350px] items-center bg-[#0f0b17]">
+        <h3 className="pb-5">NUTRITIONS</h3>
+        <span className="text-sm">
+          Sacharidy: {fruit.nutritions.carbohydrates} g
+        </span>
+        <hr className="my-2 " />
+        <span className="text-sm">
+          Kalórie: {fruit.nutritions.calories} kcal
+        </span>
+        <hr className="my-2 " />
+        <span className="text-sm">Tuky: {fruit.nutritions.fat} g</span>
+        <hr className="my-2 " />
+        <span className="text-sm">Cukor: {fruit.nutritions.sugar} g</span>
+        <hr className="my-2 " />
+        <span className="text-sm">
+          Bielkoviny: {fruit.nutritions.protein} g
+        </span>
+      </div>
+      <AddToFavorite fruit={fruit} />
+    </div>
+  );
+};
+
+export default DetailPage;
---
+++ src/components/AddToFavorite.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { TFruit } from "@/types/fruitsType";
+import React, { useEffect, useMemo, useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+
+export const AddToFavorite = ({ fruit }: { fruit: TFruit }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+    const isInLocalStorage = favorites.find(
+      (favorite: TFruit) => favorite.id === fruit.id
+    );
+    setIsFavorite(!!isInLocalStorage);
+  }, [fruit.id]);
+
+  const handleAddToFavorite = () => {
+    const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+    const isInLocalStorage = favorites.find(
+      (favorite: TFruit) => favorite.id === fruit.id
+    );
+    if (isInLocalStorage) return;
+    const newFavorites = [...favorites, fruit];
+    localStorage.setItem("favorites", JSON.stringify(newFavorites));
+    setIsFavorite(true);
+  };
+
+  return (
+    <button
+      onClick={handleAddToFavorite}
+      className=" text-white py-2 px-4 rounded-md flex items-center gap-2"
+    >
+      <FaRegHeart /> <span>Add to favorites</span>
+    </button>
+  );
+};
