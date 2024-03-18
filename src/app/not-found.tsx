83a6f6fe50bf436cc8e066ba33487617b5eb81fe
PeterZajac <zajacp011@gmail.com>
2024-03-19 00:23:12 +0100

    Add link to return back for searching a fruit in NotFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,14 +3,18 @@ import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { useWindowSize } from "react-use";
 import Confetti from "react-confetti";
+import Link from "next/link";
 
 const NotFound = () => {
   const { width, height } = useWindowSize();
 
   return (
-    <div>
+    <div className="flex-col justify-center grid place-items-center mt-40">
       <h1>NotFound</h1>
       <Confetti width={width} height={height} numberOfPieces={100} />
+      <Link className="hover:text-violet-600" href="/">
+        Return back for Searching a fruit
+      </Link>
     </div>
   );
 };
