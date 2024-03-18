86301e5a145ce16efe2a23e1e67170e585c26054
PeterZajac <zajacp011@gmail.com>
2024-03-18 18:55:00 +0100

    Add react-confetti and react-use dependencies, and update NotFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const { width, height } = useWindowSize();
+
+  return (
+    <div>
+      <h1>NotFound</h1>
+      <Confetti width={width} height={height} numberOfPieces={100} />
+    </div>
+  );
 };
 
 export default NotFound;
